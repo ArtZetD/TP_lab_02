f6fedf20fd69915c3d80b9eb4be4d3a90cbcacce
ArtZetD <creep999975@gmail.com>
2022-03-10 23:54:43 +0300

    added some comment

--- hello.cpp
+++ hello.cpp
@@ -6,7 +6,7 @@ int main()
 {
 std::string name;
 std::cout << "name - " << std::endl;
-std::cin >> name;
+std::cin >> name; // enter your name please
 std::cout << "Hello world from "<< name  << std::endl;
 
 return 0;
