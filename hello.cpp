6691f583b411f0cf9078534d63f028d6f0d2efec
ArtZetD <creep999975@gmail.com>
2022-03-11 00:19:33 +0300

    changes

--- hello.cpp
+++ hello.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <string>
 
-
-int main()
+int
+main()
 {
-std::string name;
-std::cout << "name - " << std::endl;
-std::cin >> name; // enter your name please
-std::cout << "Hello world from "<< name  << std::endl;
+  std::string name;
+  std::cout << "name - " << std::endl;
+  std::cin >> name; // enter your name please
+  std::cout << "Hello world from " << name << std::endl;
 
-return 0;
+  return 0;
 }
